83b343e5ca00abf9e7ae4bb4c2c9fdf2fc200099
n0696066 <n0696066@my.ntu.ac.uk>
2018-03-21 14:49:38 +0000

    Starting Task 5

--- src/gpx-tests/travellingTime.cpp
+++ src/gpx-tests/travellingTime.cpp
@@ -1,1 +1,12 @@
-//To be fixed
+#include <boost/test/unit_test.hpp>
+
+#include "logs.h"
+#include "route.h"
+#include "track.h"
+
+using namespace GPS;
+
+BOOST_AUTO_TEST_SUITE( Travel_Time )
+//TODO
+
+BOOST_AUTO_TEST_SUITE_END()
