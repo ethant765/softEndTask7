701b7aee38cacd6f6b7ff6ab154d3db0aada517d
n0696066 <n0696066@my.ntu.ac.uk>
2018-03-24 00:13:06 +0000

    Reverted main.cpp to original

--- src/main.cpp
+++ src/main.cpp
@@ -12,46 +12,23 @@ using namespace GPS;
 using std::cout;
 using std::endl;
 
-void generateGPXTrack(std::string trackString,std::string trackName = "", seconds Loginterval = 1,
-                      seconds timeUnitDuration = 10, seconds absoluteStartTime = 0, GridWorld gridWorld = GridWorld())
+void testRoute(std::string fileName)
 {
-    const std::string filePath = LogFiles::GPXTracksDir + trackName + ".gpx";
+    const std::string filePath = LogFiles::GPXRoutesDir + fileName + ".gpx";
 
-    std::ofstream out (filePath);
+    const bool isFileName = true;
 
-    GridWorldTrack gridWorldRoute = GridWorldTrack(trackString,timeUnitDuration,absoluteStartTime, gridWorld);
-    out << gridWorldRoute.toGPX(Loginterval,true,trackName);
-    out.close();
-    cout << "file " + filePath + " written" << endl;
-}
+    Route route = Route(filePath, isFileName);
 
-void testTrack(std::string fileName)
-{
-  const std::string filePath = LogFiles::GPXTracksDir + fileName + ".gpx";
-
-  const bool isFileName = true;
-
-  Track track = Track(filePath,isFileName);
-
-  track.travellingTime();
+    cout << "Route name: " << route.name() << endl;
+    // cout << "Number of positions in route: " << route.numPositions() << endl;
+    cout << "Total route length: " << route.totalLength() << endl;
+    // cout << "Net route length: " << route.netLength() << endl;
+    cout << endl;
 }
 
 int main()
 {
-    //GridWorld gridWorld = GridWorld(Earth::Pontianak);
-    generateGPXTrack("A1B2C", "TrackDuration1",1,1);
-    generateGPXTrack("A3B1C", "TrackDuration10",1,10);
-    generateGPXTrack("A4B2C", "TrackDurationEven",1,2);
-    generateGPXTrack("M3A2G", "TrackDurationOdd",1,7);
-    generateGPXTrack("R4U2A","TrackI&D1",1,1);
-    generateGPXTrack("A2U4R","TrackInterval10",10);
-    generateGPXTrack("M3A2G","TrackIntervalNegative",-1,1);
-    generateGPXTrack("M3A2G","TrackIntervalEven&lessthanhalf",5,20);
-    generateGPXTrack("M3A2G","TrackI&DMultiplesOdd",3,9);
-    generateGPXTrack("M3A2G","TrackI&DNotMultiples",3,7);
-    generateGPXTrack("A3B1C", "TrackAbsoluteTime1", 1,10,1);
-    generateGPXTrack("A3B1C", "TrackAbsoluteTimeNegative", 1,10,-1);
-    generateGPXTrack("A7X2E1U4M2V5G", "TrackMultiplePoints", 100,1000);
-    generateGPXTrack("A3A2B1M", "TrackHasRests", 1,10);
-    generateGPXTrack("A0A2M2M", "TrackHas0andRests", 1,10);
+    testRoute("NorthYorkMoors");
+    testRoute("ABCD");
 }
