1b0933bea4915a1e3abc85e0d335e16a047245a7
Farid Boudedja <farid.boudedja@gmail.com>
2018-03-24 17:25:46 +0000

    Tests Route::operator[](): Check if it returns all Positions of a Route

--- src/gpx-tests/array-operator.cpp
+++ src/gpx-tests/array-operator.cpp
@@ -27,6 +27,44 @@
 
 #include <boost/test/unit_test.hpp>
 
+#include "logs.h"
+#include "route.h"
+#include "track.h"
+#include "position.h"
+
+using namespace GPS;
+
 BOOST_AUTO_TEST_SUITE( Route_array_operator )
+
+/**
+ * Check that Route::operator[] returns all positions of a Route correctly
+ * The test Route is extracted from a GPX file generated using the GridWorld
+ * class. Thus all the Position values are known.
+ * Since Position does not include the == operator, its attributes are checked
+ * individually.
+ **/
+BOOST_AUTO_TEST_CASE( correct_index_route ) {
+    Route route = Route(LogFiles::GPXRoutesDir + "MIOT.gpx", true);
+
+    Position pos0 = Position(0, 109.322, 0);
+    BOOST_CHECK_EQUAL(route[0].latitude(), pos0.latitude());
+    BOOST_CHECK_EQUAL(route[0].longitude(), pos0.longitude());
+    BOOST_CHECK_EQUAL(route[0].elevation(), pos0.elevation());
+
+    Position pos1 = Position(0.089982, 109.412, 0);
+    BOOST_CHECK_EQUAL(route[1].latitude(), pos1.latitude());
+    BOOST_CHECK_EQUAL(route[1].longitude(), pos1.longitude());
+    BOOST_CHECK_EQUAL(route[1].elevation(), pos1.elevation());
+
+    Position pos2 = Position(0, 109.502, 0);
+    BOOST_CHECK_EQUAL(route[2].latitude(), pos2.latitude());
+    BOOST_CHECK_EQUAL(route[2].longitude(), pos2.longitude());
+    BOOST_CHECK_EQUAL(route[2].elevation(), pos2.elevation());
+
+    Position pos3 = Position(-0.089982, 109.502, 0);
+    BOOST_CHECK_EQUAL(route[3].latitude(), pos3.latitude());
+    BOOST_CHECK_EQUAL(route[3].longitude(), pos3.longitude());
+    BOOST_CHECK_EQUAL(route[3].elevation(), pos3.elevation());
+}
 
 BOOST_AUTO_TEST_SUITE_END()
