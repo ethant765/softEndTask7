084a4ea807249573df33fe4538fac9e1d6faf875
Edward Ross <n0685556@ntu.ac.uk>
2018-03-25 17:05:44 +0100

    Addition of some comments.

--- src/gpx-tests/n0693165.cpp
+++ src/gpx-tests/n0693165.cpp
@@ -10,33 +10,42 @@ const bool isFileName = true;
 BOOST_AUTO_TEST_SUITE( MaxRateOfDescentTests )
 
 //Correctness logic tests
-/*The first set of test cases that should be tested for are the correctness tests. These tests are for making sure that the logic is correct within the function. For example if the function was made up of:
-    a + b = c
-    return c
-then with an input of a = 2 and b = 3 there should be an output of 5.
-In this particular function the point of it is to return the greatest change between two different points in a track, but only for descent.
+/* The first set of test cases that should be tested for are the correctness tests. These tests are for making sure that the logic is correct within the function. For example if the function was made up of:
+
+        a + b = c
+        return c
+
+ * then with an input of a = 2 and b = 3 there should be an output of 5.
+ * In this particular function the point of it is to return the greatest change between two different points in a track, but only for descent.
 */
 BOOST_AUTO_TEST_CASE( correctnesstest_zero_elevation )
 {
    BOOST_CHECK_EQUAL( Track(LogFiles::GPXTracksDir + "A1B3C.gpx", isFileName).maxRateOfDescent(), 0.0 );
 }
-
+/* In these test cases the tracks will only have parts where the elevation changes to be lower at points.
+ * The output will be different based on the track and will have to be calculated before testing.
+ */
 BOOST_AUTO_TEST_CASE( correctnesstests_elevation_down )
 {
     BOOST_CHECK_EQUAL( Track(LogFiles::GPXTracksDir + "changingelevationnegative.gpx", isFileName).maxRateOfDescent(), -4.0 );
 }
-
+/* In these test cases the tracks will only have parts where the elevation changes to be higher at points.
+ * The expected output should be zero.
+ */
 BOOST_AUTO_TEST_CASE( correctnesstests_elevation_up )
 {
     BOOST_CHECK_EQUAL( Track(LogFiles::GPXTracksDir + "changingelevationpositive.gpx", isFileName).maxRateOfDescent(), 0.0 );
 }
 
-//
+//Underflow and overflow tests
+/* In this test case the track will have an initial value of the largest value double.
+ */
 BOOST_AUTO_TEST_CASE( largest_value_initial )
 {
 
 }
-
+/* In this test case the track will have an initial value of the lowest value double
+ */
 BOOST_AUTO_TEST_CASE( largest_value_subtract )
 {
 
