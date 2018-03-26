1eeba0d1ff18eb1aa3a0d16931eda6e28e3f5e73
jg71ff1n <jcgriffin999@gmail.com>
2018-03-26 19:12:30 +0100

    Merge of branch into master

--- src/gpx-tests/N0680192.cpp
+++ src/gpx-tests/N0680192.cpp
@@ -11,14 +11,18 @@ BOOST_AUTO_TEST_SUITE( findPosistion )
 
 const bool isFileName = true;
 
-// Check that the correct error is thrown when a point that is not in the list is passed
+/* Check that the correct error is thrown when a point that is not in the list is passed
+ * This to check that standard function behaviour
+ */
 BOOST_AUTO_TEST_CASE( search_for_missing_entry )
 {
    Route route = Route(LogFiles::GPXRoutesDir + "N0680192_MissingEntry.gpx", isFileName);
    BOOST_CHECK_THROW( route.findPosition("C"), std::out_of_range );
 }
 
-// Check that the correct error is thrown when a unnamed point is passed in
+/* Check that the correct error is thrown when a unnamed point is passed in
+ * This stops undefinded data errors arising
+*/
 BOOST_AUTO_TEST_CASE( unnamed_posistion )
 {
    Route route = Route(LogFiles::GPXRoutesDir + "N0680192_MissingName.gpx", isFileName);
@@ -26,6 +30,7 @@ BOOST_AUTO_TEST_CASE( unnamed_posistion )
 }
 
 // Check that the correct error is thrown when passing a lowercase value in when all values are Capitalised
+// Ensures complete strings are checked
 BOOST_AUTO_TEST_CASE( lowercase_match_error )
 {
    Route route = Route(LogFiles::GPXRoutesDir + "N0680192_StringHandling_LowerCase.gpx", isFileName);
@@ -33,6 +38,7 @@ BOOST_AUTO_TEST_CASE( lowercase_match_error )
 }
 
 // Check that the correct error is thrown when passing a uppercase value in when all values are lowercase
+// Same reasoning as previous case
 BOOST_AUTO_TEST_CASE( uppercase_match_error )
 {
    Route route = Route(LogFiles::GPXRoutesDir + "N0680192_StringHandling_UpperCase.gpx", isFileName);
@@ -40,6 +46,7 @@ BOOST_AUTO_TEST_CASE( uppercase_match_error )
 }
 
 // Check that a partial match will not make a full match
+// Checks for whole string matching and strings are not partially matched
 BOOST_AUTO_TEST_CASE( partial_match_error )
 {
    Route route = Route(LogFiles::GPXRoutesDir + "N0680192_PartialMatch.gpx", isFileName);
@@ -47,11 +54,14 @@ BOOST_AUTO_TEST_CASE( partial_match_error )
 }
 
 //Multiple BOOST_CHECK_EQUAL statements due to Position not supporting == operator
-//Check to see that repeated data is found properly
+/*Check to see that repeated data is found properly, should find the first instance of a posistion as
+ * it should search from the begining of the list and stop after a match is found to stop pointless
+ * searching of a list
+ */
 BOOST_AUTO_TEST_CASE( repeated_data_check )
 {
    Route route = Route(LogFiles::GPXRoutesDir + "N0680192_RepeatedData.gpx", isFileName);
-   Position repeatedPosition = route[5];
+   Position repeatedPosition = route[1];
    BOOST_CHECK_EQUAL( route.findPosition("B").latitude(), repeatedPosition.latitude());
    BOOST_CHECK_EQUAL( route.findPosition("B").longitude(), repeatedPosition.longitude());
    BOOST_CHECK_EQUAL( route.findPosition("B").elevation(), repeatedPosition.elevation());
