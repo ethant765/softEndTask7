b88dc0852f5aa3ac604736147d17dbe706123540
Callum Roberts <n0689529@MAE215-17-linux.ads.ntu.ac.uk>
2018-03-28 14:11:47 +0100

    fix branch to merge

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+
+#include "logs.h"
+#include "route.h"
+#include "track.h"
+#include "gridworld_route.h"
+#include "gridworld_track.h"
+
+using namespace GPS;
+
+using std::cout;
+using std::endl;
+
+void testRoute(std::string fileName)
+{
+    const std::string filePath = LogFiles::GPXRoutesDir + fileName + ".gpx";
+
+    const bool isFileName = true;
+
+    Route route = Route(filePath, isFileName);
+
+    cout << "Route name: " << route.name() << endl;
+    // cout << "Number of positions in route: " << route.numPositions() << endl;
+    cout << "Total route length: " << route.totalLength() << endl;
+    // cout << "Net route length: " << route.netLength() << endl;
+    cout << endl;
+}
+
+int main()
+{
+    testRoute("NorthYorkMoors");
+    testRoute("ABCD");
+}
+
