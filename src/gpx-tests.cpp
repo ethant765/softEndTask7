713004c5b2951d0c3c970ee42c3caa470ac25800
Tom Webb <n0682255@MAE015-18-linux.ads.ntu.ac.uk>
2018-03-28 10:22:17 +0100

    Started the first test in gpx-tests.cpp

--- src/gpx-tests.cpp
+++ src/gpx-tests.cpp
@@ -8,18 +8,19 @@
 #include "logs.h"
 #include "route.h"
 
-using namespce GPS;
+using namespace GPS;
 
 BOOST_AUTO_TEST_SUITE(MinimumLatitudeTests)
 
 BOOST_AUTO_TEST_CASE(CheckSingleValue)
 {
-    Route = (LogFiles::GPXRoutesDir +  "singleValueN0682255.gpx", true);
-
-
+    const bool isFileName = true;
+    const std::string filePath = LogFiles::GPXRoutesDir + "singleValueN0682255.gpx";
 
     Route route = Route(filePath, isFileName);
-    BOOST_CHECK_EQUAL(Route::minLatitude() , degrees 10)
+
+    BOOST_CHECK_CLOSE(route.minLatitude() , 0);
+
 }
 
 BOOST_AUTO_TEST_SUITE_END()
