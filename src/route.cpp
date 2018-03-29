fa29ea7916edff0d53d2db727c05a47b5d7c76cf
Steven <N0693955@my.ntu.ac.uk>
2018-03-29 13:04:04 +0100

    added route.cpp that was deleted by mistake

---
+++ src/route.cpp
@@ -0,0 +1,348 @@
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <cassert>
+#include <cmath>
+#include <stdexcept>
+
+#include "xmlparser.h"
+#include "geometry.h"
+#include "earth.h"
+#include "route.h"
+#include "position.h"
+using namespace GPS;
+
+std::string Route::name() const
+{
+   return routeName.empty() ? "Unnamed Route" : routeName;
+}
+
+unsigned int Route::numPositions() const
+{
+    const bool implemented = false;
+    assert(implemented);
+}
+
+metres Route::totalLength() const
+{
+    return routeLength;
+}
+
+metres Route::netLength() const
+{
+    const bool implemented = true;
+    assert(implemented);
+
+
+
+    metres distance = distanceBetween(positions[0],positions.back());
+    return distance;
+}
+
+metres Route::totalHeightGain() const
+{
+<<<<<<< HEAD
+    const bool implemented = true;
+    assert(implemented);
+
+    metres totalHeight = 0;
+
+    for(unsigned int i = 1; i < positions.size(); i++)
+    {
+        metres temp = positions[i].elevation() - positions[i-1].elevation();
+
+        if(temp > 0)
+        {
+           totalHeight += temp;
+        }
+
+    }
+    return totalHeight;
+=======
+    metres heightGain = 0;
+    if (positions.size() > 1)
+    {
+        for (unsigned int i = 1; i<positions.size(); ++i)
+        {
+            if (positions[i].elevation()-positions[i-1].elevation()>0)
+            {
+                heightGain += positions[i].elevation()-positions[i-1].elevation();
+            }
+        }
+    }
+    return heightGain;
+>>>>>>> 2baea8d5f4d2242c51d39fe2287a030d07e6ee59
+}
+
+metres Route::netHeightGain() const
+{
+    std::vector<Position>::const_iterator first, last;
+    first = positions.begin();
+    last =  positions.end() - 1;
+    if (last->elevation() - first->elevation() > 0){
+        return last->elevation() - first->elevation();
+    }
+    else{
+        return 0;
+    }
+}
+
+degrees Route::minLatitude() const
+{
+    const bool implemented = false;
+    assert(implemented);
+}
+
+degrees Route::maxLatitude() const
+{
+    const bool implemented = false;
+
+    assert(implemented);
+}
+
+degrees Route::minLongitude() const
+{
+    const bool implemented = false;
+    assert(implemented);
+}
+
+degrees Route::maxLongitude() const
+{
+    const bool implemented = false;
+
+    assert(implemented);
+
+}
+
+metres Route::minElevation() const
+{
+    degrees minElev = positions[0].elevation();
+    for(auto pos : positions){
+        if(pos.elevation() < minElev)
+            minElev = pos.elevation();
+    }
+    return minElev;
+}
+
+metres Route::maxElevation() const
+{
+<<<<<<< HEAD
+    const bool implemented = false;
+    assert(implemented);
+=======
+    if (positions.size() == 0)
+    {
+        throw std::invalid_argument("No positions in provided route");
+    }    
+    degrees maxElev = positions[0].elevation();
+    for(auto pos : positions){
+        if(pos.elevation() > maxElev)
+            maxElev = pos.elevation();
+    }
+    return maxElev;
+>>>>>>> 2baea8d5f4d2242c51d39fe2287a030d07e6ee59
+}
+
+degrees Route::maxGradient() const
+{
+    const bool implemented = false;
+    assert(implemented);
+<<<<<<< HEAD
+=======
+    //
+    degrees largestGradient = atan((positions[1].elevation() - positions[0].elevation())/distanceBetween(positions[1],positions[0]));
+    //
+    degrees testGradient = atan((positions[1].elevation() - positions[0].elevation())/distanceBetween(positions[1],positions[0]));
+
+    for(size_t x = 2; x < positions.size(); x++){
+
+          testGradient = atan((positions[x].elevation() - positions[x-1].elevation())/distanceBetween(positions[x],positions[x-1]));
+
+          if(testGradient > largestGradient){
+             largestGradient = testGradient;
+         }
+     }
+    return largestGradient;
+>>>>>>> 2baea8d5f4d2242c51d39fe2287a030d07e6ee59
+}
+
+degrees Route::minGradient() const
+{
+    const bool implemented = true;
+    assert(implemented);
+
+    if (positions.size()<2)
+    {
+        throw std::invalid_argument("Gradient cannot be worked out when using only one point");
+    }
+
+    degrees minGradient=sqrt(pow( distanceBetween(positions[0], positions[1]),2) + pow(positions[0].elevation() - positions[1].elevation(),2));
+    degrees temp, deltaH,deltaV;
+
+    if (positions[0].elevation() > positions[1].elevation())
+    {
+        minGradient = -minGradient;
+    }
+    for (unsigned int i = 2; i < positions.size(); ++i )
+    {
+        deltaH = distanceBetween(positions[i-1], positions[i]);
+        deltaV = positions[i-1].elevation() - positions[i].elevation();
+        temp= sqrt(pow(deltaH,2) + pow(deltaV,2));
+
+        if(positions[i-1].elevation() > positions[i].elevation())
+        {
+            temp = -temp;
+        }
+
+        if(temp<minGradient)
+        {
+            minGradient=temp;
+        }
+    }
+    return minGradient;
+
+}
+
+degrees Route::steepestGradient() const
+{
+    const bool implemented = false;
+    assert(implemented);
+}
+
+Position Route::operator[](unsigned int idx) const
+{
+    const bool implemented = false;
+    assert(implemented);
+}
+
+Position Route::findPosition(const std::string & soughtName) const
+{
+    const bool implemented = false;
+    assert(implemented);
+}
+
+std::string Route::findNameOf(const Position & soughtPos) const
+{
+    const bool implemented = false;
+    assert(implemented);
+}
+
+unsigned int Route::timesVisited(const std::string & soughtName) const
+{
+    const bool implemented = false;
+    assert(implemented);
+}
+
+unsigned int Route::timesVisited(const Position & soughtPos) const
+{
+    const bool implemented = false;
+    assert(implemented);
+}
+
+std::string Route::buildReport() const
+{
+    return report;
+}
+
+Route::Route(std::string source, bool isFileName, metres granularity)
+{
+    using namespace std;
+    using namespace XML::Parser;
+    string lat,lon,ele,name,temp,temp2;
+    metres deltaH,deltaV;
+    ostringstream oss,oss2;
+    unsigned int num;
+    this->granularity = granularity;
+    if (isFileName){
+        ifstream fs(source);
+        if (! fs.good()) throw invalid_argument("Error opening source file '" + source + "'.");
+        oss << "Source file '" << source << "' opened okay." << endl;
+        while (fs.good()) {
+            getline(fs, temp);
+            oss2 << temp << endl;
+        }
+        source = oss2.str();
+    }
+    if (! elementExists(source,"gpx")) throw domain_error("No 'gpx' element.");
+    temp = getElement(source, "gpx");
+    source = getElementContent(temp);
+    if (! elementExists(source,"rte")) throw domain_error("No 'rte' element.");
+    temp = getElement(source, "rte");
+    source = getElementContent(temp);
+    if (elementExists(source, "name")) {
+        temp = getAndEraseElement(source, "name");
+        routeName = getElementContent(temp);
+        oss << "Route name is: " << routeName << endl;
+    }
+    num = 0;
+    if (! elementExists(source,"rtept")) throw domain_error("No 'rtept' element.");
+    temp = getAndEraseElement(source, "rtept");
+    if (! attributeExists(temp,"lat")) throw domain_error("No 'lat' attribute.");
+    if (! attributeExists(temp,"lon")) throw domain_error("No 'lon' attribute.");
+    lat = getElementAttribute(temp, "lat");
+    lon = getElementAttribute(temp, "lon");
+    temp = getElementContent(temp);
+    if (elementExists(temp, "ele")) {
+        temp2 = getElement(temp, "ele");
+        ele = getElementContent(temp2);
+        Position startPos = Position(lat,lon,ele);
+        positions.push_back(startPos);
+        oss << "Position added: " << startPos.toString() << endl;
+        ++num;
+    } else {
+        Position startPos = Position(lat,lon);
+        positions.push_back(startPos);
+        oss << "Position added: " << startPos.toString() << endl;
+        ++num;
+    }
+    if (elementExists(temp,"name")) {
+        temp2 = getElement(temp,"name");
+        name = getElementContent(temp2);
+    }
+    positionNames.push_back(name);
+    Position prevPos = positions.back(), nextPos = positions.back();
+    while (elementExists(source, "rtept")) {
+        temp = getAndEraseElement(source, "rtept");
+        if (! attributeExists(temp,"lat")) throw domain_error("No 'lat' attribute.");
+        if (! attributeExists(temp,"lon")) throw domain_error("No 'lon' attribute.");
+        lat = getElementAttribute(temp, "lat");
+        lon = getElementAttribute(temp, "lon");
+        temp = getElementContent(temp);
+        if (elementExists(temp, "ele")) {
+            temp2 = getElement(temp, "ele");
+            ele = getElementContent(temp2);
+            nextPos = Position(lat,lon,ele);
+        } else nextPos = Position(lat,lon);
+        if (areSameLocation(nextPos, prevPos)) oss << "Position ignored: " << nextPos.toString() << endl;
+        else {
+            if (elementExists(temp,"name")) {
+                temp2 = getElement(temp,"name");
+                name = getElementContent(temp2);
+            } else name = ""; // Fixed bug by adding this.
+            positions.push_back(nextPos);
+            positionNames.push_back(name);
+            oss << "Position added: " << nextPos.toString() << endl;
+            ++num;
+            prevPos = nextPos;
+        }
+    }
+    oss << num << " positions added." << endl;
+    routeLength = 0;
+    for (unsigned int i = 1; i < num; ++i ) {
+        deltaH = distanceBetween(positions[i-1], positions[i]);
+        deltaV = positions[i-1].elevation() - positions[i].elevation();
+        routeLength += sqrt(pow(deltaH,2) + pow(deltaV,2));
+    }
+    report = oss.str();
+}
+
+void Route::setGranularity(metres granularity)
+{
+    bool implemented = false;
+    assert(implemented);
+}
+
+bool Route::areSameLocation(const Position & p1, const Position & p2) const
+{
+    return (distanceBetween(p1,p2) < granularity);
+}
