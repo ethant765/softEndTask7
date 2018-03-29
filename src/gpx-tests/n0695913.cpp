45b132743f6f17857dc05bc406dc615aac7bb3b5
Jonathan Mason <jm0804@protonmail.ch>
2018-03-29 16:10:10 +0100

    Replaced old test files with single test file. Updated Boost test and added more tests

--- src/gpx-tests/n0695913.cpp
+++ src/gpx-tests/n0695913.cpp
@@ -34,36 +34,36 @@ BOOST_AUTO_TEST_CASE( correct_num_positions_route_oneposition )
 // Check that the number of positions is correct for a route with a granularity of 10 metres
 BOOST_AUTO_TEST_CASE( correct_num_positions_route_granularity10m )
 {
-   Route route = Route(LogFiles::GPXRoutesDir + "n0695913_granularity10m.gpx", numCorrect, 10);
-   BOOST_CHECK_EQUAL( route.numPositions(), 10 );
+   Route route = Route(LogFiles::GPXRoutesDir + "n0695913_granularityall.gpx", numCorrect, 10);
+   BOOST_CHECK_EQUAL( route.numPositions(), 9 );
 }
 
 // Check that the number of positions is correct for a route with a granularity of 20 metres
 BOOST_AUTO_TEST_CASE( correct_num_positions_route_granularity20m )
 {
-   Route route = Route(LogFiles::GPXRoutesDir + "n0695913_granularity20m.gpx", numCorrect, 20);
-   BOOST_CHECK_EQUAL( route.numPositions(), 4 );
+   Route route = Route(LogFiles::GPXRoutesDir + "n0695913_granularityall.gpx", numCorrect, 20);
+   BOOST_CHECK_EQUAL( route.numPositions(), 9 );
 }
 
-/*// Check that the number of positions is correct for a route with a granularity of 50 metres
+// Check that the number of positions is correct for a route with a granularity of 50 metres
 BOOST_AUTO_TEST_CASE( correct_num_positions_route_granularity50m )
 {
-   Route route = Route(LogFiles::GPXRoutesDir + "N0695913_granularity50m.gpx", numCorrect, 50);
-   BOOST_CHECK_EQUAL( route.numPositions(), 5 );
+   Route route = Route(LogFiles::GPXRoutesDir + "n0695913_granularityall.gpx", numCorrect, 50);
+   BOOST_CHECK_EQUAL( route.numPositions(), 7 );
 }
 
 // Check that the number of positions is correct for a route with a granularity of 100 metres
 BOOST_AUTO_TEST_CASE( correct_num_positions_route_granularity100m )
 {
-   Route route = Route(LogFiles::GPXRoutesDir + "N0695913_granularity100m.gpx", numCorrect, 100);
-   BOOST_CHECK_EQUAL( route.numPositions(), 5 );
+   Route route = Route(LogFiles::GPXRoutesDir + "n0695913_granularityall.gpx", numCorrect, 100);
+   BOOST_CHECK_EQUAL( route.numPositions(), 6 );
 }
 
 // Check that the number of positions is correct for a route with a granularity of 200 metres
 BOOST_AUTO_TEST_CASE( correct_num_positions_route_granularity200m )
 {
-   Route route = Route(LogFiles::GPXRoutesDir + "N0695913_granularity200m.gpx", numCorrect, 200);
-   BOOST_CHECK_EQUAL( route.numPositions(), 5 );
-}*/
+   Route route = Route(LogFiles::GPXRoutesDir + "n0695913_granularityall.gpx", numCorrect, 200);
+   BOOST_CHECK_EQUAL( route.numPositions(), 4 );
+}
 
 BOOST_AUTO_TEST_SUITE_END()
